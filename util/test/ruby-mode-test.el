8b5dc1c07316559fac6d3be0e23ffcec6ae21cc9
Nathan Weizenbaum <nex342@gmail.com>
2008-11-24 16:50:23 -0800

    Add more here-doc tests.

--- util/test/ruby-mode-test.el
+++ util/test/ruby-mode-test.el
@@ -21,11 +21,59 @@
   (test-here-doc-p "
 <<FOO
 <here-doc>
+  FOO
+<here-doc>
 FOO
 <no-here-doc>
 <<BAR
 <here-doc>
 BAR"))
+
+(ert-deftest ruby-here-doc-p-whitespace ()
+  (test-here-doc-p "
+<<-FOO
+<here-doc>
+  FOO
+<no-here-doc>
+<<-BAR
+<here-doc>
+BAR
+<no-here-doc>"))
+
+(ert-deftest ruby-here-doc-p-quotes ()
+  (test-here-doc-p "
+<<\"FOO\"
+<here-doc>
+FOO
+<no-here-doc>
+
+<<'FOO'
+<here-doc>
+FOO
+<no-here-doc>
+
+<<'FOO BAR'
+<here-doc>
+FOO BAR
+<no-here-doc>
+
+<<'-'
+<here-doc>
+-
+<no-here-doc>
+
+<<' '
+<here-doc>
+ 
+<no-here-doc>")
+  ;; This is failing :(
+;;;   (test-here-doc-p "<<''
+;;; <here-doc>
+;;; foo
+;;; <here-doc>
+;;; 
+;;; <no-here-doc>")
+)
 
 (ert-deftest ruby-here-doc-p-multiple ()
   (test-here-doc-p "
